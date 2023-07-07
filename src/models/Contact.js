fccf8f8e7f1a93ade2cbec449987b5d319ad7d37
Kanishkweb <129086096+Kanishkweb@users.noreply.github.com>
2023-07-07 22:24:14 +0530

    Update Contact.js
    Add comments and fix an bug

--- src/models/Contact.js
+++ src/models/Contact.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
-const Contact = mongoose.Schema({
+const Contact = new mongoose.Schema({
     email:String,
     phone:String,
     query:String
 })
 
-module.exports= mongoose.model('queries',Contact)
+module.exports= mongoose.model('queries',Contact)  // First Argument model name and second argument Schema name
